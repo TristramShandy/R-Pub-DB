39fd6b3db2b385c5847209e2d83121fedc872a7f
Dr. Michael Ulm <michael@mulm.at>
2011-10-23 11:08:50 -0400

    First commit of pristine rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpubdb::Application.config.secret_token = '5757d0c3ce1041045a384aa0f77b31b1c194ba54d23b9adee8b9ba5155bb906c17f27d88ee0ee6bb4c9c47a993076549b1d3bd66d5853c54c5890eac98f863ed'
