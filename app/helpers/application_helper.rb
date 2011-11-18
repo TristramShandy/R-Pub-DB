a2b6052e9be4e3341eb8590ab135f7b3b1f9a77d
Dr. Michael Ulm <michael@mulm.at>
2011-11-18 07:30:55 -0500

    Implemented helper function for sortable table headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
   # returns link for a sortable header of list items
   def sortable_header(header_item)
-    # ... #
+    title = t(header_item.name)
+    css_class = (header_item.attribute == session[:sort_column] ? "current #{session[:sort_direction]}" : nil)
+    direction = (header_item.attribute == session[:sort_column] && session[:sort_direction] == :asc ? :desc : :asc)
+    link_to title, {:sort => header_item.attribute.to_s, :direction => direction.to_s}, {:class => css_class}
   end 
 
   # returns path to edit method of a sortable item
