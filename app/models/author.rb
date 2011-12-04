586953c82d1c62e9bfa8cfa7124d39fe63dea6ee
Dr. Michael Ulm <michael@mulm.at>
2011-12-04 16:00:54 +0100

    Added item filters

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,4 +8,7 @@ class Author < ActiveRecord::Base
     Sortable::HeaderInfo.new(:last_name,        :last_name,       Sortable::C_Default, Sortable::C_Default),
     Sortable::HeaderInfo.new(:affiliation,      :affiliation,     Sortable::C_Yes,     Sortable::C_Yes    )]
 
+  def display_name
+    "#{first_name} #{last_name}"
+  end
 end
