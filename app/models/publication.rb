80dda0a437636462edc9f17b62cab0024d2a2bbf
Dr. Michael Ulm <michael@mulm.at>
2012-01-02 09:48:49 +0100

    Changed publication year to sortable and filterable

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -20,7 +20,7 @@ class Publication < ActiveRecord::Base
     Sortable::HeaderInfo.new(:source,           :source,          Sortable::C_No,      Sortable::C_No     ),
     Sortable::HeaderInfo.new(:volume,           :volume,          Sortable::C_No,      Sortable::C_No     ),
     Sortable::HeaderInfo.new(:number,           :number,          Sortable::C_No,      Sortable::C_No     ),
-    Sortable::HeaderInfo.new(:year,             :year,            Sortable::C_No,      Sortable::C_No     ),
+    Sortable::HeaderInfo.new(:year,             :year,            Sortable::C_Yes,     Sortable::C_Yes    ),
     Sortable::HeaderInfo.new(:pages,            :pages,           Sortable::C_No,      Sortable::C_No     ),
     Sortable::HeaderInfo.new(:doi,              :doi,             Sortable::C_No,      Sortable::C_No     ),
     Sortable::HeaderInfo.new(:abstract,         :abstract,        Sortable::C_No,      Sortable::C_Yes    ),
